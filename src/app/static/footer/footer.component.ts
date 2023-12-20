db701f621effec014c602e70dd4ab12e40b135f7
mite <mite@gmail.com>
2023-12-20 13:00:31 +0530

    Created components

---
+++ src/app/static/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-footer",
+    standalone:true,
+    imports:[],
+    templateUrl:'./footer.component.html',
+    styleUrls:[]
+})
+
+export class FooterComponent {
+    
+}
---
+++ src/app/static/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-navbar',
+    standalone:true,
+    imports:[],
+    templateUrl:'./navbar.component.html',
+    styleUrls:[]
+})
+
+export class NavbarComponent {
+
+}
