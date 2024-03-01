a9ccf5f08dddde61e53c70b9501421cc04acda1b
uttamthegoat <19516uttam@gmail.com>
2024-03-01 21:15:28 +0530

    changed styles

--- src/app/student-info/student-info.component.ts
+++ src/app/student-info/student-info.component.ts
@@ -7,9 +7,13 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './student-info.component.html',
-  styleUrl: './student-info.component.css'
+  styleUrl: './student-info.component.css',
 })
 export class StudentInfoComponent {
-  studentInfo:any=data
-  studentCount=this.studentInfo.length
+  studentInfo: any = data;
+  studentCount = this.studentInfo.length;
+  showStudents: any = [];
+  getStudents(cnt: string) {
+    this.showStudents = this.studentInfo.slice(0, Number(cnt));
+  }
 }
